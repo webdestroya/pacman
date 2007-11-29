bbc5695314ac73f239aeb83ab97cb60781c5c644
webdestroya <mrdempsey@gmail.com>
2007-11-29 17:14:53 +0000

    * work on the server test

--- server/pacManServer.pl
+++ server/pacManServer.pl
@@ -3,6 +3,11 @@
 # This is a makeshift server to emulate another computer making moves.
 # Use this to make sure that the game is behaving the way it should be.
 
+# Packet Info (All joined with |)
+# ----------
+# Type
+#
+
 use Socket;
 
 ($port) = @ARGV;
@@ -40,13 +45,18 @@ for(; ;)
 		# fork out to the kid
 		print "PacMan connected!\n";
 
-		($af,$port,$inetaddr) = unpack('C4', $inetaddr);
-
+		($af,$port,$inetaddr) = unpack($sockaddr, $addr);
+		@inetaddr = unpack('C4', $inetaddr);
 		print "Connection Dump: $af $port @inetaddr\n";
 
 		while(<NS>)
 		{
 			print "Connection: $_";
+			$comm = chomp($_);
+			@comm = split(/|/, $comm);
+			
+
+
 			print(NS '$_');
 		}
 		close(NS);
