9935671a57bf331c5b818bde59dd761aa7eb0e96
Michael Yeganyan <michaely23@uci.edu>
2007-12-02 23:27:31 +0000

    GUI Team added revised scoreBoard with new graphics; added "lives" sprites with pac-men as life indicators

--- code/uci/pacman/gui/ScoreBoard.java
+++ code/uci/pacman/gui/ScoreBoard.java
@@ -7,10 +7,11 @@ import code.uci.pacman.game.GameState;
 import code.uci.pacman.game.PacManGame;
 import code.uci.pacman.objects.stationary.Fruit;
 import ucigame.*;
+import java.util.*;
 
 /**
  * 
- * @author GUI Team
+ * @author GUI Team (Rick, Mike, Cameron)
  * this will display the score, lives, and current fruit
  *
  */
@@ -19,23 +20,56 @@ public class ScoreBoard{
 	private int remainingLives = 0;
 	private Fruit currentFruit = null;
 	private Sprite score;
+	private Sprite lives; 
+	private int scorePosX = 0;
+	private int scorePosY = 610;
+	private int livesPosX = 300;
+	private int livesPosY = 610;
+	private int lifePosX = 355;
+	private int lifePosY = 610;
+	private int lifePosAdj = 30;
+	ArrayList<Sprite> lifeList = new ArrayList();
 	
 	public ScoreBoard() {
-		// Hey we dicked with your shit. 
-		// Scorebaord can't extend sprite it's gotta be a collection of sprites
+		// Hey we provided an example of how to use gameInstances to apply sprites. 
+		// Scoreboard can't extend sprite it's gotta be a collection of sprites
 		// so you need to make multiple sprites in here
 		// TODO Auto-generated constructor stub
+		
+		//create score sprite
 		score = GameController.getInstance().getPacInstance().makeSpriteFromPath("images/final/score.png");
-		score.position(0, 610);
+		score.position(scorePosX, scorePosY);
 		score.font("Tahoma", PacManGame.BOLD, 24, 255, 255, 255);
+		
+		//create lives sprite
+		lives = GameController.getInstance().getPacInstance().makeSpriteFromPath("images/final/lives.png");
+		lives.position(livesPosX, livesPosY);
+		lives.font("Tahoma", PacManGame.BOLD, 24, 255, 255, 255);
+
+		//set remainLives
+		remainingLives = GameState.getInstance().getLives();
+		for(int x = 1; x <= remainingLives; x++){
+			lifeList.add(GameController.getInstance().getPacInstance().makeSpriteFromPath("images/final/life.png"));
+		}
 	}
 	
 	public void draw(){
 		//this needs to draw all the stuff that you make
 		// to get all of the information use GameState.getInstance().getTheItemYouWant()
-		score.putText(GameState.getInstance().getScore()+"", 120,26);
+		
+		//add scores from GameState and draw score sprite onto canvas
+		score.putText(GameState.getInstance().getScore() + "", 120,26);
 		score.draw();
+		
+		//add lives from GameState and draw life sprite onto canvas
+		lives.draw();
+		remainingLives = GameState.getInstance().getLives();
+		for(int x = 1; x <= remainingLives; x++){
+			lifeList.get(x-1).position(lifePosX + x*lifePosAdj, lifePosY);
+			lifeList.get(x-1).draw();
+		}
 	}
+	
 	
 	/**
 	 * 
