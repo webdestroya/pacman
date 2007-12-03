b9afef0f7e35f78375eebf23f08dcd92854eb466
Sen Hirano <tsukeinen2@gmail.com>
2007-12-03 19:41:40 +0000

    javadocs started for objects.stationaryobject

--- code/uci/pacman/objects/StationaryObject.java
+++ code/uci/pacman/objects/StationaryObject.java
@@ -13,43 +13,77 @@ import ucigame.Sprite;
  *
  */
 public abstract class StationaryObject extends Sprite {
+	
     protected GameController control;
     protected String objectGraphicPath;
     
-	private StationaryObject(Image img, int x, int y) {
+    /**
+     * @param img
+     * @param x
+     * @param y
+     */
+    private StationaryObject(Image img, int x, int y) {
 		super(img.width(), img.height());
 		super.addFrame(img, 0, 0);
 		this.position(x, y);
 		control = GameController.getInstance();
 	}
 	
+	/**
+	 * @param path
+	 * @param x
+	 * @param y
+	 */
 	public StationaryObject(String path, int x, int y) {
 		this(getImage(path), x, y);
 		this.objectGraphicPath = path;
 	}
 	
+	/**
+	 * @param width
+	 * @param height
+	 */
 	public StationaryObject(int width, int height) {
 		super(width, height);
 		
 	}
 	
+	/**
+	 * @return
+	 */
 	public String getGraphicPath() {
 		return objectGraphicPath;
 	}
 	
+	/**
+	 * @param c
+	 * @return
+	 */
 	public boolean collidedWith(ControllableObject c){
 		super.checkIfCollidesWith(c, PacManGame.PIXELPERFECT);
 		return super.collided() && super.isShown();
 	}
 	
+	/**
+	 * @param p
+	 */
 	public void position(Point p) {
 		super.position(p.x, p.y);
 	}
 	
+	/**
+	 * @param imgPath
+	 * @param x
+	 * @param y
+	 */
 	public void addFrame(String imgPath, int x, int y){
 		super.addFrame(getImage(imgPath), x, y);
 	}
 	
+	/**
+	 * @param stringPath
+	 * @return
+	 */
 	private static Image getImage(String stringPath){
 		String resources = "images/final/";
 		return GameController.getInstance().getPacInstance().getImage(resources+stringPath);
