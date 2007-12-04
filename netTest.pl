1d833a79ea05225b8bf13c2c88144a7de1a1854a
webdestroya <mrdempsey@gmail.com>
2007-12-04 22:08:46 +0000

    * final net tester

--- netTest.pl
+++ netTest.pl
@@ -4,8 +4,8 @@ use Socket;
 socket(SOCKET, PF_INET, SOCK_DGRAM, getprotobyname("udp")) or die "socket: $!";
 
 %PTYPES = (
-	JOIN=>0,
-	GTYPE=>1,
+	JOIN=>'0',
+	GTYPE=>'1',
 	GMOVE=>2,
 	PMOVE=>3,
 	GAMEOVER=>4,
@@ -14,44 +14,46 @@ socket(SOCKET, PF_INET, SOCK_DGRAM, getprotobyname("udp")) or die "socket: $!";
 	GAMESTART=>7,
 	
 	
-	BLINKY=>0,
+	BLINKY=>'0',
 	CLYDE=>1,
 	INKY=>2,
 	PINKY=>3,
 	
 	
-	UP=>0,
+	UP=>'0',
 	DOWN=>1,
 	LEFT=>2,
-	RIGHT=>3
+	RIGHT=>3,
 );
 
 $HOSTNAME = "127.0.0.1";
 $PORTNO = 4445;
 $DATA1 = 0;
-$DATA2 = 2;
-$DATA3 = 2;
-$DATA4 = 2;
+$DATA2 = 0;
+$DATA3 = 0;
+$DATA4 = 0;
 
-if( $#ARGV>0 )
+
+if( $#ARGV>=0 )
 {
 	$DATA1 = $PTYPES{ $ARGV[0] };
 }
-if( $#ARGV>1 )
+if( $#ARGV>0 )
 {
 	$DATA2 = $PTYPES{ $ARGV[1] };
 }
-if( $#ARGV>2 )
+if( $#ARGV>1 )
 {
 	$DATA3 = $PTYPES{ $ARGV[2] };
 }
-if( $#ARGV>3 )
+if( $#ARGV>2 )
 {
 	$DATA4 = $PTYPES{ $ARGV[3] };
 }
 
 $MSG = pack('cccc', $DATA1, $DATA2, $DATA3, $DATA4);
 
+print "Message: [".$DATA1."|".$DATA2."|".$DATA3."|".$DATA4."]\n";
 print "Message: ".$MSG."\n";
 
 $ipaddr   = inet_aton($HOSTNAME);
