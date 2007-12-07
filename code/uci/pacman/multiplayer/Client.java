f94004ce532141cf8c0867e2abdd39e58511c90d
webdestroya <mrdempsey@gmail.com>
2007-12-07 00:04:48 +0000

    *client

--- code/uci/pacman/multiplayer/Client.java
+++ code/uci/pacman/multiplayer/Client.java
@@ -217,7 +217,6 @@ public class Client extends Thread
 					{
 						// set lives count
 						int lives = (buf[1]&0xFF);
-						System.out.println("RLIVES: "+lives);
 						GameState.getInstance().setLives( lives );
 					}
 					else if( PType.AFRUIT.ordinal() == packetType )
